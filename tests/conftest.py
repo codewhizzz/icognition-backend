618001a90fe985d8beccfb10c50c72c05e2b3b15
Damilare <damilaretoyese@gmail.com>
2024-02-08 14:00:59 +0000

    solution

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,10 +6,3 @@ def pytest_configure(config):
     if not os.getenv('DATABASE_URL'):
         setattr(config.option, 'markexpr', 'not requires_database')
 
-# test_app.py
-import pytest
-
-@pytest.mark.requires_database
-def test_function_that_requires_database():
-    # Your test code that requires DATABASE_URL
-
